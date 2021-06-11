fcf44f1b808d81cbbe06b60546b430b8ecf0abc9
Harsh Jain <hvj.techture@gmail.com>
2021-06-11 07:42:36 +0530

    few concepts more added

---
+++ src/Create.js
@@ -0,0 +1,42 @@
+import {useState} from 'react';
+const Create = () => {
+
+    const [title , setTitle] = useState('');
+    const [body , setBody] = useState('');
+    const [author , setAuthor] = useState('mario');
+
+    return ( 
+        <div className="create">
+            <h2> Add a new Blog </h2>
+            <form>
+            <label>Blog Title :</label>
+            <input type="text" 
+                   required
+                   value={title}
+                   onChange={(e) => setTitle(e.target.value)}
+            />
+            <label>Blog Body :</label>
+            <textarea
+                required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+            >
+            </textarea>
+            <label>Blog Author :</label>
+            <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+            >
+                <option value="mario">mario</option>
+                <option valaue="luigi">luigi</option>
+            </select>
+            <button> Add Blog </button>
+            </form>
+            <p>{title} </p>
+            <p>{body}</p>
+            <p>{author}</p>
+        </div>
+     );
+}
+ 
+export default Create;
--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,17 @@
-import { useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
 
-    const [blogs , setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const { data:blogs , isPending , error} = useFetch('http://localhost:8000/blogs');
 
-    const [name , setName] = useState('mario');
-    
-    const handleDelete = (id) => {
-        const newBlogs  = blogs.filter( blog => blog.id !== id);
-        setBlogs(newBlogs);
-    };
-
-    useEffect(() =>{
-        console.log("use Effect ran");
-        console.log(name);
-    }, [name]);
-    return (  
+    return ( 
         <div className="home">
-           <BlogList blogs={blogs} title="Blog List" handleDelete={handleDelete} />
-            <button onClick={()=> setName('luigi')}>Change Name</button>
-            <p>{ name }</p>
+        { error && <div> {error} </div> }
+        { isPending && <div> Loading....</div> } 
+        { blogs && <BlogList blogs={blogs} title="Blog List" /> }
         </div>
     );
 }
  
 export default Home;
-
-  // const handleClickAgain = () => {
-    //     console.log('Hello ' + name);
-    //     console.log('Event ',e);
-    // }
-
-
-
-    {
-        /*
-         useState example
-
-          const [name , setName] = useState('Mario');
-    const  [age , setAge] = useState(25); 
-
-    const handleClick =  () => {
-    
-    setName('luigi');
-    setAge(35);
-
-    }
-
-         <p> I'm {name} & my age is {age} </p>
-            <button onClick= { handleClick }>Click ME</button>
-             <button onClick= { (e)=> handleClickAgain('Mario', e.target)} style={{marginLeft:'8px'}}> Click Me Again</button> */}
-
-
-             {/*
-                reusing component example 
-                <BlogList blogs={blogs.filter((blog) => blog.author ==='mario')} title="Mario's Blog" /> */}
